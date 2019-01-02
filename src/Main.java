699df610077f2f47ff5d7e3b3f855ad561c537be
doni-kalando <donikalandog@gmail.com>
2019-01-02 14:03:51 +0300

    Work with files. Write to file.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,29 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String []args) {
+
+        File directory = new File ("/media/doni/Etheria/projects/idea/untitled/fileDirectory/");
+        File file_one = new File(directory, "test_docks.txt");
+
+        try(FileWriter writer = new FileWriter(file_one, false)) {
+
+            // запись всей строки
+            String text = "Hello Gold!";
+            writer.write(text);
+            // запись по символам
+            writer.append('\n');
+            writer.append('E');
+
+            writer.flush();
+
+
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+
     }
 }
