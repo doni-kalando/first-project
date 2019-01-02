9452dd2c7126f29b53bff05d3fcb2ed781fe1f67
doni-kalando <donikalandog@gmail.com>
2019-01-02 17:48:12 +0300

    Work with files. Write and Read from files. Using character stream buffering. BufferedReader and BufferedWriter

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,58 @@
-import java.io.File;
-import java.io.FileWriter;
+import java.io.*;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String []args) {
 
-        File directory = new File ("/media/doni/Etheria/projects/idea/untitled/fileDirectory/");
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("\n Please enter the directory where the file is located. \n " +
+                " (Attention! There should not be any spaces in the path). \n " +
+                " Example:  /media/doni/Etheria/projects/idea/untitled/fileDirectory/ \n " +
+                "Input a Directory:");
+        String dir = in.next();
+
+        System.out.println("\n Enter the file name. \n " +
+                " (Attention! document name must not contain spaces). \n " +
+                " Example: readable_dock.txt \n " +
+                "Input a Name file:");
+        String file_name = in.next();
+
+        File directory = new File (dir);
         File file_one = new File(directory, "test_docks.txt");
+        File file_two = new File(directory, file_name);
 
-        try(FileWriter writer = new FileWriter(file_one, false)) {
+        if(directory.exists() && file_one.exists() && file_two.exists()){
+            System.out.println("\n Directory and Files true");
+        }else {
+            System.out.println("\n Directory and Files false");
+        }
 
-            // запись всей строки
-            String text = "Hello Gold!";
-            writer.write(text);
-            // запись по символам
-            writer.append('\n');
-            writer.append('E');
+        //Считывание из файла, через буфиризацию
+        try(BufferedReader br = new BufferedReader(new FileReader(file_two)))
+        {
+            //чтение построчно
+            String s;
+            while((s=br.readLine())!=null){
 
-            writer.flush();
+                System.out.println(s);
+            }
+        }
+        catch(IOException ex){
 
+            System.out.println(ex.getMessage());
+        }
 
-        } catch (IOException ex) {
+        //Запись в файл, через буфиризацию
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(file_one)))
+        {
+            String text = " Hello  World! \n Hey! Teachers! Leave them kids alone.";
+            bw.write(text);
+        }
+        catch(IOException ex){
+
             System.out.println(ex.getMessage());
         }
 
