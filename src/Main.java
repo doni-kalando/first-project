689618805a5c17db9522b5e4b0760c8243aa5ba1
doni-kalando <donikalandog@gmail.com>
2019-01-04 02:51:38 +0300

    Use of methods.

--- src/Main.java
+++ src/Main.java
@@ -12,26 +12,40 @@ public class Main {
                 " (Attention! There should not be any spaces in the path). \n " +
                 " Example:  /media/doni/Etheria/projects/idea/untitled/fileDirectory/ \n " +
                 "Input a Directory:");
-        String dir = in.next();
+        File directory = new File (in.next());
+
 
         System.out.println("\n Enter the file name. \n " +
                 " (Attention! document name must not contain spaces). \n " +
                 " Example: readable_dock.txt \n " +
                 "Input a Name file:");
-        String file_name = in.next();
+        File file_two = new File(directory, in.next());
 
-        File directory = new File (dir);
+
         File file_one = new File(directory, "test_docks.txt");
-        File file_two = new File(directory, file_name);
+
 
         if(directory.exists() && file_one.exists() && file_two.exists()){
             System.out.println("\n Directory and Files true");
+
+            //Считывание из файла, через буфиризацию
+            getReadFile(file_two);
+
+            //Запись в файл, через буфиризацию
+            getWriteFile(file_one);
+
         }else {
             System.out.println("\n Directory and Files false");
         }
 
-        //Считывание из файла, через буфиризацию
-        try(BufferedReader br = new BufferedReader(new FileReader(file_two)))
+    }
+
+    /**
+     * Метод принимает путь и имя файла, после открывает файл и считывает его построчно, выводя результат в консоль.
+     * @param nameReadableFile
+     */
+    private static void getReadFile(File nameReadableFile){
+        try(BufferedReader br = new BufferedReader(new FileReader(nameReadableFile)))
         {
             //чтение построчно
             String s;
@@ -44,9 +58,14 @@ public class Main {
 
             System.out.println(ex.getMessage());
         }
+    }
 
-        //Запись в файл, через буфиризацию
-        try(BufferedWriter bw = new BufferedWriter(new FileWriter(file_one)))
+    /**
+     * Метод принимает путь и имя файла, после открывает файл и записывает в него строку.
+     * @param nameRecordedFile
+     */
+    private static void getWriteFile(File nameRecordedFile){
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(nameRecordedFile)))
         {
             String text = " Hello  World! \n Hey! Teachers! Leave them kids alone.";
             bw.write(text);
@@ -55,6 +74,5 @@ public class Main {
 
             System.out.println(ex.getMessage());
         }
-
     }
 }
